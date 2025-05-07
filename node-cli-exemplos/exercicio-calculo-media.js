55fd21ee86f80aa3b8ae5b570a5496658f200fd0
kauhe <kauhe25@gmail.com>
2025-05-07 10:50:59 -0300

    Exercicio calculo média

---
+++ node-cli-exemplos/exercicio-calculo-media.js
@@ -0,0 +1,9 @@
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const argv = yargs(hideBin(process.argv)).argv;
+
+//console.log(argv);
+const {nota1, nota2} = argv;
+const mediaFinal = (argv.nota1 + argv.nota2) / 2;
+
+console.log(`A média final é ${mediaFinal}.`);
