1b83fb45b199fbd1318060ad402f3272517c487f
kauhe <kauhe25@gmail.com>
2025-05-07 11:01:15 -0300

    Adição da mensagem de parabéns

--- node-cli-exemplos/exercicio-calculo-media.js
+++ node-cli-exemplos/exercicio-calculo-media.js
@@ -5,5 +5,13 @@ const argv = yargs(hideBin(process.argv)).argv;
 //console.log(argv);
 const {nota1, nota2} = argv;
 const mediaFinal = (argv.nota1 + argv.nota2) / 2;
+const aprovado = mediaFinal >= 6 ? true : false;
 
+console.log(`Nota 1: ${argv.nota1}, Nota 2: ${argv.nota2}`);
 console.log(`A média final é ${mediaFinal}.`);
+
+if(aprovado){
+    console.log("Parabéns, aluno aprovado.");
+}else{
+    console.log('Ops, aluno Reprovado');
+}
