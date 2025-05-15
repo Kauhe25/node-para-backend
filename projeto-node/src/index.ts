b937cd3a04e61d656918d51d4b04f1e078f889ea
kauhe <kauhe25@gmail.com>
2025-05-15 18:36:54 -0300

    exercicio some e horario

--- projeto-node/src/index.ts
+++ projeto-node/src/index.ts
@@ -5,6 +5,33 @@ import express, { Request, Response } from "express";
 const app = express();
 const port = 3030;
 
+// soma
+app.get('/soma', (req: Request, res: Response): void => {
+    const resultado = 40 + 60;
+    res.send(`Resultado da soma: ${resultado}`);
+})
+
+app.get('/horario', (req: Request, res: Response): void => {
+
+    res.type('html');
+    const agora = new Date();
+    const hora = agora.getHours();
+    const minuto = agora.getMinutes();
+    const segundo = agora.getSeconds();
+    let cor = "";
+    let segundoEstilizado = "";
+
+    if(segundo % 2 == 0){
+        cor = "red";
+    }else {
+        cor = "blue";
+    }
+
+    segundoEstilizado = `<a style=color:${cor}>${segundo}</a>`;
+    res.send(`<div>A hora atual é: ${hora}:${minuto}:${segundoEstilizado}</div>`);
+
+})
+
 app.get('/', (req: Request, res: Response): void => {
     const aluno = {
         nome: "Kauhe",
